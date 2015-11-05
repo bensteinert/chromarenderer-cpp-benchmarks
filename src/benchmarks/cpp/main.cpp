0e3fdb8802ee64a2b169ceda7096faa52a647912
bensteinert <benjamin.steinert@googlemail.com>
2015-11-05 18:39:32 +0100

    Enabled json output for cpp benchmarks

--- src/benchmarks/cpp/main.cpp
+++ src/benchmarks/cpp/main.cpp
@@ -1,9 +1,12 @@
 #include <hayai.hpp>
+#include <hayai_json_outputter_mod.hpp>
 
 
 int main() {
     hayai::ConsoleOutputter consoleOutputter;
     hayai::Benchmarker::AddOutputter(consoleOutputter);
+    mod_hayai::JsonOutputter jsonOutputter;
+    hayai::Benchmarker::AddOutputter(jsonOutputter);
     hayai::Benchmarker::RunAllTests();
     return 0;
 }
