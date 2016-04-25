69fcf31ede662609ec5270aac3025c39909e5ed2
bensteinert <benjamin.steinert@googlemail.com>
2016-04-25 10:18:22 +0200

    New Chromarenderer submodule. Fixes benchmark project to work with new version.

--- src/benchmarks/cpp/MoellerTrumboreTriangleIntersectionBenchmark.cpp
+++ src/benchmarks/cpp/MoellerTrumboreTriangleIntersectionBenchmark.cpp
@@ -3,8 +3,8 @@
 #include <Ray.h>
 #include <float.h>
 
-static Triangle t = Triangle(Vector3(0.f, 0.f, 1.f), Vector3(.0f, 1.f, 1.f), Vector3(1.f, 0.f, 1.f));
-static Ray ray = Ray(Vector3(0.2f, 0.2f, 0.f), Vector3(0.f, 0.f, 1.f), .0f, FLT_MAX);
+static Triangle t = Triangle(Vector3(0.f, 0.f, 1.f), Vector3(.0f, 1.f, 1.f), Vector3(1.f, 0.f, 1.f), 0, 0);
+static Ray ray = Ray(Vector3(0.2f, 0.2f, 0.f), Vector3(0.f, 0.f, 1.f), .0f, FLT_MAX, .0f);
 static float u, v = .0f;
 
 static float result;
